7b1132632c59415198e6fdf408194b7b243a6757
Ildar Vagapov <Ildar-Sarbai@Rambler.ru>
2023-02-03 21:19:53 +0300

    надо допилить, дописать

---
+++ chetvertay/Program.cs
@@ -0,0 +1,10 @@
+﻿// напишите программу, которая принимает на вход число (N) и 
+// выдает таблицу квадратов чисел от 1 до N 
+// 5 -> 25
+// 2 -> 4, 
+Console.Clear();
+Console.Write("Введите X: ");
+int X = int.Parse(Console.ReadLine());
+
+Console.Write("Введите Y: ");
+int Y = int.Parse(Console.ReadLine());
