b7e5311bf8c2f649d0fd9f0d01c340bf8447c846
Ildar Vagapov <Ildar-Sarbai@Rambler.ru>
2023-02-03 23:39:45 +0300

    написал программу рабочую

---
+++ vtoray0/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+Console.Write("введите номер четверти: ");
+int quarter = int.Parse(Console.ReadLine());
+switch (quarter)
+{
+  case 1:
+    {
+      Console.WriteLine("x>0 и y>0");
+      break;
+    }
+  case 2:
+    {
+      Console.WriteLine("x<0 и y>0");
+      break;
+    }
+  case 3:
+    {
+      Console.WriteLine("x<0 и y<0");
+      break;
+    }
+  case 4:
+    {
+      Console.WriteLine("x>0 и y<0");
+      break;
+    }
+  default:
+    {
+      Console.WriteLine("введена не правильная четверть!!!");
+      break;
+    }
+}
